fff9462457a6897a5cf513b23550a031f6bc19c5
Debangshi Pramanik <debangshipramanik84@gmail.com>
2025-07-30 15:02:05 +0530

    Add files via upload

---
+++ insertion element_in_middle.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Solution:
+    def insertInMiddle(self, head, x):
+        if head is None:
+            return Node(x)
+        
+        slow = head
+        fast = head.next
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        new_node = Node(x)
+        new_node.next = slow.next
+        slow.next = new_node
+        
+        return head
+
